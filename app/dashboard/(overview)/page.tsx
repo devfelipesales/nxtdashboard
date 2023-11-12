2ce54329e4fde1b4b09652d37f342dbd20e98ecf
Felipe Sales <felipefsp.dev@gmail.com>
2023-11-11 21:01:21 -0300

    feat: added skeleton component to dashboard page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import { fetchLatestInvoices, fetchRevenue } from '../../lib/data';
 
 export default async function DashboardPage() {
   const revenue = await fetchRevenue();
